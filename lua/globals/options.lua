b49cd1ed4daee4e6776d5f575579019ee8e56105
wilberquito <wbq.software@gmail.com>
2022-10-09 19:18:57 +0200

    Highligh at yank

--- lua/globals/options.lua
+++ lua/globals/options.lua
@@ -36,3 +36,11 @@ vim.opt.listchars = {  -- see hidden chars and their colors
 vim.o.list = true
 vim.o.updatetime = 100
 
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+    augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+    augroup END
+]]
+
