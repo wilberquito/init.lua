1db4b65d41f8fab2b2c615607463a71be114e2ee
wilberquito <wbq.software@gmail.com>
2022-10-20 21:43:29 +0200

    remap to move lines in visual model

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -1,10 +1,17 @@
-options = { noremap = true }
+local options = { noremap = true }
+local cmd_options = { noremap = true, silent = true }
+
+local map = vim.api.nvim_set_keymap
 
 -- Change panes on vim ctrl mappings
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', options)
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', options)
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', options)
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', options)
+map('n', '<C-h>', '<C-w>h', options)
+map('n', '<C-j>', '<C-w>j', options)
+map('n', '<C-k>', '<C-w>k', options)
+map('n', '<C-l>', '<C-w>l', options)
+
+-- Move lines up/down
+map("v", "<C-j>", ":m '>+1<CR>gv=gv", cmd_options)
+map("v", "<C-k>", ":m '<-2<CR>gv=gv", cmd_options)
 
 -- Escape maps to a rolling jk
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', options)
+map('i', 'jk', '<ESC>', options)
