e67b2c8a13d4c06eac945628a1289e12d057021b
wilberquito <wbq.software@gmail.com>
2022-10-24 22:31:10 +0200

    change nvim theme

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -34,7 +34,7 @@ return require('packer').startup(function()
         'projekt0n/github-nvim-theme',
         config = function()
             require('github-theme').setup({
-                theme_style = "dark_default"
+                theme_style = "dimmed"
             })
         end
     })
