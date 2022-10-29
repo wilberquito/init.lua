270251924c78059e212fe58d3b781d8078ff219b
wilberquito <wbq.software@gmail.com>
2022-10-29 19:56:08 +0200

    cursor blinks in insert mode

--- lua/globals/options.lua
+++ lua/globals/options.lua
@@ -36,6 +36,8 @@ vim.opt.listchars = {  -- see hidden chars and their colors
 vim.o.list = true
 vim.o.updatetime = 100
 
+vim.o.guicursor='i:blinkon1' -- let cursor blink in insert mode
+
 -- highlight yanked text for 200ms using the "Visual" highlight group
 vim.cmd[[
     augroup highlight_yank
@@ -54,4 +56,5 @@ vim.cmd[[
         augroup END
     endif
 ]]
+
 
