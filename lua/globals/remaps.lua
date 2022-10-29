3abe44c7ec6fd4a7d7c5019ad4a720f3e23faf66
wilberquito <wbq.software@gmail.com>
2022-10-29 14:11:08 +0200

    adds remaps

--- lua/globals/remaps.lua
+++ lua/globals/remaps.lua
@@ -1,36 +1,44 @@
-local options = { noremap = true }
-local cmd_options = { noremap = true, silent = true }
+local nr_opts = { noremap = true }
+local opts = { noremap = true, silent = true }
 
 local map = vim.api.nvim_set_keymap
 
 -- Change panes on vim ctrl mappings
-map('n', '<C-h>', '<C-w>h', options)
-map('n', '<C-j>', '<C-w>j', options)
-map('n', '<C-k>', '<C-w>k', options)
-map('n', '<C-l>', '<C-w>l', options)
+map('n', '<C-h>', '<C-w>h', nr_opts)
+map('n', '<C-j>', '<C-w>j', nr_opts)
+map('n', '<C-k>', '<C-w>k', nr_opts)
+map('n', '<C-l>', '<C-w>l', nr_opts)
 
 -- Save
-map("n", "<leader>fs", ":w<CR>", options)
-map("n", "<leader>fr", ":source $MYVIMRC<CR>", options)
+map("n", "<leader>fs", ":w<CR>", nr_opts)
+map("n", "<leader>fr", ":source $MYVIMRC<CR>", nr_opts)
+
+-- Quit
+map("n", "<leader>fk", ":wq<CR>", nr_opts)
+map("n", "<leader>fu", ":q!<CR>", nr_opts)
 
 -- Move lines up/down
-map("v", "<C-j>", ":m '>+1<CR>gv=gv", cmd_options)
-map("v", "<C-k>", ":m '<-2<CR>gv=gv", cmd_options)
+map("v", "<S-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<S-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Stay in indent mode
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- Escape maps to a rolling jk
-map('i', 'jk', '<ESC>', options)
+map('i', 'jk', '<ESC>', nr_opts)
 
 -- Center result search
-map("n", "n", "nzz", cmd_options)
-map("n", "N", "Nzz", cmd_options)
+map("n", "n", "nzz", opts)
+map("n", "N", "Nzz", opts)
 
--- Easy way to enter in command instructions
-map("n", ";", ":", cmd_options)
-map("n", ":", ";", cmd_options)
+-- Move between paragraphs
+map("n", "}", "{", opts)
+map("n", "{", "}", opts)
+map("v", "}", "{", opts)
+map("v", "{", "}", opts)
 
--- Easy way to enter in command instructions
-map("n", "}", "{", cmd_options)
-map("n", "{", "}", cmd_options)
-map("v", "}", "{", cmd_options)
-map("v", "{", "}", cmd_options)
+-- Navigate buffers
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
 
