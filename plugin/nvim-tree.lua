293d089d84b4ad8b22c1672a56d09a6b1e8cdf00
wilberquito <wbq.software@gmail.com>
2022-10-29 14:19:08 +0200

    adds control for nvim tree

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,3 +1,9 @@
-require'nvim-tree'.setup()
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+
+nvim_tree.setup()
 
 vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle <CR>', { noremap = true })
