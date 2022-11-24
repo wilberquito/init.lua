6e9173ed6aea8a129c260f17e8b7d7ddb669304d
wilberquito <wbq.software@gmail.com>
2022-11-24 11:14:33 +0100

    Adds haskell language server as configuration in lsp

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,3 +1,9 @@
+local ok, config = pcall(require, "lspconfig")
+if not ok then
+    error("lsp config is not installed")
+	return
+end
+
 local options = { noremap = true }
 
 -- LSP config mappings
@@ -10,3 +16,23 @@ vim.api.nvim_set_keymap('n', '<leader>ds', '<cmd>lua vim.lsp.buf.signature_help(
 vim.api.nvim_set_keymap('n', '<leader>dn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', options)
 vim.api.nvim_set_keymap('n', '<leader>dp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', options)
 
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+config['hls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    autostart = true
+}
+
+-- config['pyright'].setup{
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+-- }
+-- config['tsserver'].setup{
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+-- }
