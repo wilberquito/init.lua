c2c46955d1ffa40e2ede623c8c704005c3e94157
wilberquito <wbq.software@gmail.com>
2022-12-16 23:06:29 +0100

    Rename files

--- lua/wilberquito/init.lua
+++ lua/wilberquito/init.lua
@@ -1,7 +1,7 @@
 -- Leader key is a space
 vim.g.mapleader = " "
 
-require("wilberquito.option")
-require("wilberquito.remaps")
+require("wilberquito.set")
+require("wilberquito.remap")
 require("wilberquito.packer")
 
