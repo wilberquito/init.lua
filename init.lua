515fc78be21267d0d420f380e2e486dda5224e95
wilberquito <wbq.software@gmail.com>
2022-12-16 22:53:32 +0100

    User package level

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
--- Leader key is a space
-vim.g.mapleader = " "
-
--- Setup globals that should always be around
-require 'globals/options'
-require 'globals/remaps'
- 
--- Install plugins
--- o
-require 'packer/plugins'
+require('wilberquito')
---
+++ lua/wilberquito/init.lua
@@ -0,0 +1,7 @@
+-- Leader key is a space
+vim.g.mapleader = " "
+
+require("wilberquito.option")
+require("wilberquito.remaps")
+require("wilberquito.packer")
+
