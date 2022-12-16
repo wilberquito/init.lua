62179ac981e61e1239292b493e06c340245b855b
wilberquito <wbq.software@gmail.com>
2022-12-17 00:05:01 +0100

    Use function to set color scheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,7 @@
-function ColorMyPencils(color)
-    color = color or "gruvbox"
-    vim.cmd.colorscheme(color)
+function ColorMyWorld(color)
+    vim.cmd.colorscheme(color or "gruvbox")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end 
 
-ColorMyPencils()
+ColorMyWorld("gruvbox")
