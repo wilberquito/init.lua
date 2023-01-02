7bf2998a62f761ae631265fd06bc79fe1fd8e90d
wilberquito <wbq.software@gmail.com>
2023-01-02 23:07:11 +0100

    Formats code

--- lua/wilberquito/packer.lua
+++ lua/wilberquito/packer.lua
@@ -6,28 +6,28 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function()
-	-- Packer just manages itself
-	use 'wbthomason/packer.nvim'
+    -- Packer just manages itself
+    use 'wbthomason/packer.nvim'
 
-	-- nvim LSP configs
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'williamboman/mason.nvim'
+    -- nvim LSP configs
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'williamboman/mason.nvim'
 
-	----------------
-	-- Plugin development
-	----------------
+    ----------------
+    -- Plugin development
+    ----------------
 
     -- remember cursor last place
     use 'ethanholz/nvim-lastplace'
 
-	-- Luasnip (primarily for hrsh7th/nvim-cmp)
-	use 'L3MON4D3/LuaSnip'
-	use 'saadparwaiz1/cmp_luasnip'
+    -- Luasnip (primarily for hrsh7th/nvim-cmp)
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
 
     use { "ellisonleao/gruvbox.nvim" }
 
@@ -37,36 +37,36 @@ return require('packer').startup(function()
     -- Catppuccin theme
     use { "catppuccin/nvim", as = "catppuccin" }
 
-	-- Treesitter is life
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+    -- Treesitter is life
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-	use 'nvim-treesitter/playground'
+    use 'nvim-treesitter/playground'
 
-	-- Gaze deeply into the unknown
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Gaze deeply into the unknown
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	--Use fzf native for telescope
-	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	
-	-- nvim file tree. Alternative to NerdTree
-	use {
-		'kyazdani42/nvim-tree.lua',
-		requires = {
-			'kyazdani42/nvim-web-devicons', -- optional, for file icon
-		},
-		config = function() require'nvim-tree'.setup {} end
-	}
+    --Use fzf native for telescope
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-	-- Git stuff
-	use 'airblade/vim-gitgutter'
+    -- nvim file tree. Alternative to NerdTree
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+            'kyazdani42/nvim-web-devicons', -- optional, for file icon
+        },
+        config = function() require'nvim-tree'.setup {} end
+    }
 
-	-- comment stuff out
+    -- Git stuff
+    use 'airblade/vim-gitgutter'
+
+    -- comment stuff out
     use {
         'numToStr/Comment.nvim',
         config = function()
@@ -76,40 +76,40 @@ return require('packer').startup(function()
 
     use 'folke/zen-mode.nvim'
 
-	-- Time Pope is a god
-	use 'tpope/vim-surround'
-	use 'tpope/vim-fugitive'
+    -- Time Pope is a god
+    use 'tpope/vim-surround'
+    use 'tpope/vim-fugitive'
 
-	-- Yanking manager (yeah yeah I know, registers exist)
-	use {
-		"AckslD/nvim-neoclip.lua",
-		requires = { {'nvim-telescope/telescope.nvim'} },
-		config = function()
-			require('neoclip').setup()
-		end
-	}
+    -- Yanking manager (yeah yeah I know, registers exist)
+    use {
+        "AckslD/nvim-neoclip.lua",
+        requires = { {'nvim-telescope/telescope.nvim'} },
+        config = function()
+            require('neoclip').setup()
+        end
+    }
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 
-	use {
-		'goolord/alpha-nvim',
-		requires = { 'kyazdani42/nvim-web-devicons' },
-		config = function ()
-			require'alpha'.setup(require'alpha.themes.theta'.config)
-		end
-	}
+    use {
+        'goolord/alpha-nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.theta'.config)
+        end
+    }
 
-	-- Typescript. Oh Typescript. Where art thou Typescript.
-	use 'jose-elias-alvarez/null-ls.nvim'
-	use 'jose-elias-alvarez/nvim-lsp-ts-utils'
+    -- Typescript. Oh Typescript. Where art thou Typescript.
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'jose-elias-alvarez/nvim-lsp-ts-utils'
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
