a44ae73dc716e56740c4e62e12eab4fc2567ffc9
wilberquito <wbq.software@gmail.com>
2023-01-04 21:53:54 +0100

    Renames vairable

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -69,11 +69,11 @@ local setup = {
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+local options = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, options)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, options)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, options)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, options)
 
 
 require("lspconfig").sumneko_lua.setup(setup)
