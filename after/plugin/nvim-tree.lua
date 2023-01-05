58972ecd35d15fa58e16baa363e78a101f779c62
wilberquito <wbq.software@gmail.com>
2023-01-05 19:05:26 +0100

    Simplifies logic and silent when nvim tree is toggled
    closes #1, #3

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,9 +1,31 @@
-local ok, nvim_tree = pcall(require, "nvim-tree")
+local ok, conf = pcall(require, "nvim-tree")
 if not ok then
     error("ERROR - nvim-tree not found")
     return
 end
 
-nvim_tree.setup()
 
-vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle <CR>', { noremap = true })
+conf.setup({
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+})
+
+
+local opts = { noremap = true, silent = true }
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle <CR>', opts)
+
