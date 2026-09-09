ea56c4773e1e15efed14a1eff5b5560dddd349a0
wilberquito <typingwil@gmail.com>
2026-09-09 12:11:49 +0200

    feat: mechanism for copying and pasting nvim in remote ssh connections

--- init.lua
+++ init.lua
@@ -24,6 +24,20 @@ vim.opt.tabstop = 4           -- Number of spaces a tab counts for
 vim.opt.shiftwidth = 4        -- Spaces used for autoindent
 vim.opt.softtabstop = 4       -- Spaces inserted when pressing Tab
 
+-- clipboard configuration for remote ssh connections
+if os.getenv("SSH_CONNECTION") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
 
 require("lazy").setup("plugins")
 
