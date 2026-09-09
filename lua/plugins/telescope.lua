9439b7dea17498e53f62ea4ab1842d9c4f6ae019
wilberquito <typingwil@gmail.com>
2026-09-09 12:09:49 +0200

    feat: support for listing vim buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,6 +35,10 @@ return {
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
                 desc = "Live grep",
             })
+
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+                desc = "Buffers",
+            })
         end,
     }
 }
